e8e95bc0a982a0560f2e8fa8e9a7fbbaef8ccb20
KANG MYUNGJOO <kmjk1117@gmail.com>
2024-07-13 00:23:42 +0900

    Title: 길이 단위 변환하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240713/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 30.48 * 9.2;
+        double b = 160934 * 1.3;
+        System.out.printf("9.2ft = %.1fcm%n", a);
+        System.out.printf("1.3mi = %.1fcm%n", b);
+    }
+}
