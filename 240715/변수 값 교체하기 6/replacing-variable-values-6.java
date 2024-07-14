a0bbafafd63c078eaa9d26815da276b23a5b0fb2
KANG MYUNGJOO <kmjk1117@gmail.com>
2024-07-15 00:14:37 +0900

    Title: 변수 값 교체하기 6, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =2, b=5,temp;
+
+        temp = a;
+        a= b;
+        b=temp;
+        System.out.print(a+"\n"+b);
+
+    }
+}
