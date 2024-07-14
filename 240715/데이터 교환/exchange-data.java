6f00dccb087d0c138d7a81c6753d786a3ff3e61e
KANG MYUNGJOO <kmjk1117@gmail.com>
2024-07-15 00:19:02 +0900

    Title: 데이터 교환, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/데이터 교환/exchange-data.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5,b=6,c=7,temp;
+        temp = a;
+        a = c;
+        c = b;
+        b = temp;
+        System.out.print(a+"\n"+b+"\n"+c);
+    }
+}
