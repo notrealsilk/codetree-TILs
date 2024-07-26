635635f9aaca9a516e44f281887499999e9c38f4
notrealsilk <kmjk1117@gmail.com>
2024-07-26 23:08:17 +0900

    Title: 최소공배수 구하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240726/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,3 +1,20 @@
+# 최대공약수 구하는 함수
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+# 최소공배수 구하는 함수
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+n, m = map(int, input().split())
+
+# 100 이하의 최소공배수를 찾기
+result = lcm(n, m)
+print(result)
+
+'''
 n, m = map(int, input().split())
 
 i = 1
@@ -7,3 +24,4 @@ while i <= 100:
         break
     i += 1
 print(result)
+'''
