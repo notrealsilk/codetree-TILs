35b05966851ff0f60c06352d4fa33ece30a59c47
notrealsilk <kmjk1117@gmail.com>
2024-07-27 19:24:06 +0900

    Title: 시험 통과 여부 확인하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240727/시험 통과 여부 확인하기/verify-test-passed.java
+++ 240727/시험 통과 여부 확인하기/verify-test-passed.java
@@ -9,7 +9,7 @@ public class Main {
             System.out.println("pass");
         }
         else {
-            System.out.println(a+"more score");
+            System.out.println((80-a)+ " more score");
         }
     }
 }
