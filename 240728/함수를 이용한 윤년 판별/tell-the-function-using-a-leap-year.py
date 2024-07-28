3f1a3ace2297f306e5ef42d2d403d2eb8854dd17
notrealsilk <kmjk1117@gmail.com>
2024-07-28 18:31:13 +0900

    Title: 함수를 이용한 윤년 판별, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240728/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240728/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,8 @@
 def year(n):
     if n % 4 == 0:
+        if n % 100 == 0 and n % 400 != 0:
+            return "false"
         return "true"
-    if n % 100 == 0 and n % 400 != 0:
-        return "false"
     else :
         return "false"
 
