a23f26694da4a82d7ceef45d3631e1f08d6e3def
notrealsilk <kmjk1117@gmail.com>
2024-08-01 01:04:38 +0900

    Title: 사칙연산 함수, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/사칙연산 함수/quadratic-operations-function.py
+++ 240801/사칙연산 함수/quadratic-operations-function.py
@@ -18,14 +18,12 @@ c = int(c)
 
 
 if o == '+':
-    result = plus(a, c)
+    print(f"{a} {o} {c} = {plus(a, c)}")
 elif o == '-':
-    result = minus(a, c)
+    print(f"{a} {o} {c} = {minus(a, c)}")
 elif o == '*':
-    result = mul(a, c)
+    print(f"{a} {o} {c} = {mul(a, c)}")
 elif o == '/':
-    result = div(a, c)
+    print(f"{a} {o} {c} = {div(a, c)}")
 else:
-    result = 'False'
-
-print(f"{a} {o} {c} = {result}")
+    print('False')
