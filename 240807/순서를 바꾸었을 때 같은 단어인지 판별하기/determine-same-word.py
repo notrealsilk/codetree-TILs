966cce061a5924537033160138ed33db91e57181
notrealsilk <kmjk1117@gmail.com>
2024-08-07 22:32:26 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 2351ms, Memory: 31MB, Status: Passed - CodeTree

--- 240807/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
+++ 240807/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -1,12 +1,18 @@
 a = list(map(str,input()))
 b = list(map(str,input()))
 
-for i in a :
-    if i in b :
-        b.remove(i)
+def word(a,b):
+    if len(a) != len(b): # 문자열 길이가 다니면 아예 x
+        return "No"
 
-# for문이 끝났을 때 b가 비어있으면 a,b는 동일한 알파벳 ㅇ
-if b == []:
-    print("Yes")
-else :
-    print("No")
+    for i in a :
+        if i in b :
+            b.remove(i)
+
+    # for문이 끝났을 때 b가 비어있으면 a,b는 동일한 알파벳 ㅇ
+    if b == []:
+        return "Yes"
+    else :
+        return "No"
+
+print(word(a,b))
