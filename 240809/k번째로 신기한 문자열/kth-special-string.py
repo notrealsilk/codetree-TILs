279e78c6648b29950562f7a08ff664aff173b23b
notrealsilk <kmjk1117@gmail.com>
2024-08-09 21:15:16 +0900

    Title: k번째로 신기한 문자열, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/k번째로 신기한 문자열/kth-special-string.py
+++ 240809/k번째로 신기한 문자열/kth-special-string.py
@@ -1,9 +1,12 @@
 n, k, T = map(str,input().split())
 word = []
+result_word = []
 
 # 단어 입력받고 리스트로 만들기
 for i in range(int(n)):
     word.append(input())
+    if word[i][:2] == T:
+        result_word.append(word[i])
 
 word.sort()
 print(word[int(k)])
