6324fa19d75fcdc041dfc5e8892177f1c810af5f
notrealsilk <kmjk1117@gmail.com>
2024-08-13 23:09:11 +0900

    Title: 문자에 따른 명령 2, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/문자에 따른 명령 2/text-based-commands2.py
+++ 240813/문자에 따른 명령 2/text-based-commands2.py
@@ -1,12 +1,18 @@
-dirt = input()
-dir_num = 3
 x, y = 0, 0
-dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+dir_num = 3 #북쪽
 
-for i in dirt:
-    if i == "L":
-        dir_num = (dir_num - 1) % 4
-    elif i == "F":
-        x, y = x + dx[dir_num], y + dy[dir_num]
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+
+dir_input = input()
+
+for cdir in dir_input:
+    if cdir == 'L':
+        dir_num = (dir_num - 1) % 4  # 왼쪽으로 90도 회전
+    elif cdir == 'R':
+        dir_num = (dir_num + 1) % 4  # 오른쪽으로 90도 회전
+    else:
+        x += dx[dir_num]  # x축 방향으로 한 칸 이동
+        y += dy[dir_num]  # y축 방향으로 한 칸 이동
 
 print(x, y)
